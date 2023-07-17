2143d07e0032ab979d06a17c0bdc03736b8b62f2
Anand Suresh <133847835+CodeWithAnandSuresh@users.noreply.github.com>
2023-07-17 11:51:47 +0530

    Rename 13_practice_program.py to 013_practice_program.py

--- 013_practice_program.py
+++ 013_practice_program.py
@@ -45,3 +45,20 @@ num1 = num1 - num2
 # Printing the values of num1 and num2 after swapping
 print(f"The value of num1 before swap: {num1}")
 print(f"The value of num2 before swap: {num2}")
+# ---------------------------------------------------------------------
+
+# METHOD3: WITHOUT USING A TEMP PLACEHOLDER
+# Input dynamic numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Printing the value of num1 and num2 before swapping
+print(f"The value of num1 before swap: {num1}")
+print(f"The value of num2 before swap: {num2}")
+
+# Swapping the values of num1 and num2 without a temp variable
+num1, num2 = num2, num1
+
+# Printing the values of num1 and num2 after swapping
+print(f"The value of num1 before swap: {num1}")
+print(f"The value of num2 before swap: {num2}")
